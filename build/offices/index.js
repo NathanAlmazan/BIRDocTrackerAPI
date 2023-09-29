d97f60d310a94791bdf2862497ec0bdd411adb63
= <=>
2023-09-29 20:37:25 +0800

    removed build folder

--- build/offices/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.officesMutationFields = exports.officesQueryFields = void 0;
-var views_1 = require("./views");
-Object.defineProperty(exports, "officesQueryFields", { enumerable: true, get: function () { return views_1.queryFields; } });
-var views_2 = require("./views");
-Object.defineProperty(exports, "officesMutationFields", { enumerable: true, get: function () { return views_2.mutationFields; } });
-//# sourceMappingURL=index.js.map
--- build/pubsub/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const graphql_redis_subscriptions_1 = require("graphql-redis-subscriptions");
-const pubsub = new graphql_redis_subscriptions_1.RedisPubSub();
-exports.default = pubsub;
-//# sourceMappingURL=index.js.map
